ca62c734f2a9d165eb41d59fc691bfb13a23a45b
hemanta4291 <hemanta25roy@gmail.com>
2019-07-24 18:57:40 +0600

    Updates

---
+++ precache-manifest.2959112c796d0043c431be0b9beafb90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f34f89fe863e13a6372f76e3c545e80",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "ec051d3fdde11012b39f",
+    "url": "/production/static/css/main.e2aa93ee.chunk.css"
+  },
+  {
+    "revision": "4fbf7d9d91980afe851d",
+    "url": "/production/static/js/2.71bfdff6.chunk.js"
+  },
+  {
+    "revision": "ec051d3fdde11012b39f",
+    "url": "/production/static/js/main.d1f42714.chunk.js"
+  },
+  {
+    "revision": "6af66b1dc346966c06ee",
+    "url": "/production/static/js/runtime~main.2910c19d.js"
+  }
+]);
